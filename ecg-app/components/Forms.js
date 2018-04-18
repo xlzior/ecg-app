8a9162abd274f94490de576db82036b3afd0c2aa
annnnnnnnnnie <2330610648@qq.com>
2018-04-18 17:57:11 +0800

    Prepared to try to embed a form in this component
    It seems that google forms are not really supported?

--- ecg-app/components/Forms.js
+++ ecg-app/components/Forms.js
@@ -8,3 +8,6 @@ export default class Forms extends React.Component {
 }
 
 const styles = StyleSheet.create({});
+//reference materials: 
+//https://medium.com/react-native-development/easily-build-forms-in-react-native-9006fcd2a73b
+//https://github.com/gcanti/tcomb-form-native
