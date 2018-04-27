acc6ee1f3d0c431a2b5d0a70b1dd31a158b7a90b
wenjunlye <30801910+wenjunlye@users.noreply.github.com>
2018-04-27 21:01:56 +0800

    Implement search function in UniversityListContainer

--- ecg-app/components/UniversityListContainer.js
+++ ecg-app/components/UniversityListContainer.js
@@ -1,10 +1,60 @@
 import React, { Component } from "react";
 import { AsyncStorage } from "react-native";
-import { Container, Content, Text, Header, Body, Title, List, ListItem } from "native-base";
+import { Container, Content, Text, Header, Body, Title, Form, Item, Input } from "native-base";
 
 import UniversityList from "./UniversityList";
 
 export default class UniversityListContainer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchTerm: "",
+      universities: []
+    }
+  }
+
+  static getDerivedStateFromProps(props) {
+    return { universities: props.universities.slice() }
+  }
+
+  handleKeypress(searchTerm) {
+    searchTerm = searchTerm.toLowerCase()
+    if (searchTerm == "") {
+      this.setState({
+        searchTerm,
+        universities: this.props.universities.slice()
+      })
+      return true;
+    }
+    // update university list by filtering for unis that match the search term
+    var filteredUnis = [];
+    if (this.props.universities.length > 0) {
+      var {universities} = this.props;
+      universities.forEach(uni => {
+        var filteredFacs = uni.faculties.filter(({details}) => {
+          for (var key in details) {
+            var searchArea = details[key].toLowerCase();
+            if (searchArea.indexOf(searchTerm) != -1) {
+              return true;
+            }
+          }
+        });
+        if (filteredFacs.length > 0) {
+          filteredUnis.push({
+            id: uni.id,
+            name: uni.name,
+            faculties: filteredFacs
+          })
+        }
+      })
+    }
+
+    this.setState({
+      searchTerm,
+      universities: filteredUnis
+    });
+  }
+
   render() {
     return (
       <Container>
@@ -14,7 +64,16 @@ export default class UniversityListContainer extends Component {
           </Body>
         </Header>
         <Content>
-          <UniversityList universities={this.props.universities}/>
+          <Form>
+            <Item>
+              <Input
+                placeholder="Search"
+                onChangeText={t => this.handleKeypress(t)}
+                value={this.state.searchTerm}
+              />
+            </Item>
+          </Form>
+          <UniversityList universities={this.state.universities}/>
         </Content>
       </Container>
     )
