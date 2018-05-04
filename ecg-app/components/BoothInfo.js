7a16676b2d250291a4013d43fd147d1030e82437
wenjunlye <30801910+wenjunlye@users.noreply.github.com>
2018-05-04 20:20:59 +0800

    Update university and faculty pages to suit new requirements
    Separate out university and faculty pages into two separate components

--- ecg-app/components/BoothInfo.js
+++ ecg-app/components/BoothInfo.js
@@ -24,11 +24,42 @@ export default class BoothInfo extends Component {
   render() {
     var {showModal, toggleModal, type, info} = this.props;
     if (type == "faculty") {
-      var {Courses, Desc, IGP, Name, Picture, University, Website} = info.details;
+      var display = (
+        <FacultyInfo
+          data={info.details}
+          universities={this.state.universities}
+          type={type}
+          toggleModal={b=>toggleModal(b)}
+        />
+      )
     } else if (type == "university") {
-      var {Desc, Faculties, Name, Picture, ShortName, Website} = this.state.universities[info.id];
+      var display = (
+        <UniversityInfo
+          data={this.state.universities[info.id]}
+          faculties={this.state.faculties}
+          type={type}
+          toggleModal={b=>toggleModal(b)}
+        />
+      )
     }
     
+    return (
+      <Modal
+        animationType="slide"
+        transparent={false}
+        visible={showModal}
+      >
+        {display}
+      </Modal>
+    )
+  }
+}
+
+class UniversityInfo extends Component {
+  render() {
+    var {data, faculties, type, toggleModal} = this.props;
+    var {Desc, Faculties, Name, Picture, ShortName, Website, IGP, Courses, Scholarships, Admissions, Prerequisites} = data;
+
     // close button for faculties and unis
     var closeButton = (
       <Button
@@ -40,22 +71,109 @@ export default class BoothInfo extends Component {
       </Button>
     )
 
-    var title = <Text>Title</Text>
-    // title for faculties
-    if (type == "faculty") {
-      University = this.state.universities[University].ShortName;
-      var title = (
+    // title for unis
+    var title = <Text style={[styles.header, styles.marginBottom]}>{Name} ({ShortName})</Text>;
+
+    // picture for faculties and unis
+    var image = Picture ? (
+      <Image
+        style={[styles.image, styles.marginBottom]}
+        source={{uri: Picture}}
+      />
+    ) : null;
+
+    // description for faculties and unis
+    var description = Desc ? (
+      <Text style={[styles.marginBottom, styles.desc]}>{Desc}</Text>
+    ) : null;
+
+    // website for faculties and unis
+    var website = Website ? (
+      <Hyperlink
+        linkDefault={true}
+        linkStyle={styles.link}
+        style={styles.marginBottom}
+      >
+        <Text>Website: {Website}</Text>
+      </Hyperlink>
+    ) : null;
+
+    // faculties for unis
+    if (type == "university") {
+      var facultiesList = [];
+      for (var id in Faculties) {
+        facultiesList.push(faculties[Faculties[id]]);
+      }
+      var faculties = (
         <View>
-          <Text style={styles.header}>{University}</Text>
-          <Text style={[styles.header, styles.marginBottom]}>{Name}</Text>
+          <Text>Faculties</Text>
+          {facultiesList.map(fac => {
+            return <ListItem><Text>{fac.Name}</Text></ListItem>
+          })}
         </View>
       )
     }
 
-    // title for unis
-    else if (type == "university") {
-      var title = <Text style={[styles.header, styles.marginBottom]}>{Name} ({ShortName})</Text>;
+    // links for unis
+    if (type == "university") {
+      var linksDisplay = []
+      var links = {
+        "IGP": IGP,
+        "Courses": Courses,
+        "Scholarships": Scholarships,
+        "Admissions Requirements": Admissions,
+        "'A'-Level Subject Pre-requisites": Prerequisites
+      }
+
+      for (let key in links) {
+        if (links[key]) linksDisplay.push(
+          <Hyperlink
+            linkDefault={true}
+            linkStyle={styles.link}
+            style={styles.marginBottom}
+            linkText={ url => key }
+          >
+            <Text>{links[key]}</Text>
+          </Hyperlink>
+        )
+      }
     }
+    return (
+      <ScrollView style={styles.container}>
+        {closeButton}
+        {title}
+        {image}
+        {description}
+        {linksDisplay}
+        <List>{faculties}</List>
+      </ScrollView>
+    )
+  }
+}
+
+class FacultyInfo extends Component {
+  render() {
+    var {data, universities, type, toggleModal} = this.props;
+    var {Courses, Desc, IGP, Name, Picture, University, Website} = data;
+    // close button for faculties and unis
+    var closeButton = (
+      <Button
+        small transparent dark
+        onPress={()=>toggleModal(false)}
+        style={styles.closeButton}
+      >
+        <Icon name="close"/>
+      </Button>
+    )
+
+    // title for faculties
+    University = universities[University].ShortName;
+    var title = (
+      <View>
+        <Text style={styles.header}>{University}</Text>
+        <Text style={[styles.header, styles.marginBottom]}>{Name}</Text>
+      </View>
+    )
 
     // picture for faculties and unis
     var image = Picture ? (
@@ -79,31 +197,15 @@ export default class BoothInfo extends Component {
     var website = Website ? (
       <Hyperlink
         linkDefault={true}
-        linkStyle={{color: "#2980b9"}}
+        linkStyle={styles.link}
         style={styles.marginBottom}
       >
         <Text>Website: {Website}</Text>
       </Hyperlink>
     ) : null;
 
-    // faculties for unis
-    if (type == "university") {
-      var facultiesList = [];
-      for (var id in Faculties) {
-        facultiesList.push(this.state.faculties[Faculties[id]]);
-      }
-      var faculties = (
-        <View>
-          <Text>Faculties</Text>
-          {facultiesList.map(fac => {
-            return <ListItem><Text>{fac.Name}</Text></ListItem>
-          })}
-        </View>
-      )
-    }
-
     // courses for faculties
-    var courses = Courses? (
+    var courses = (Courses && type == "faculty") ? (
       <View>
         <Text>Courses</Text>
         <List>
@@ -118,22 +220,15 @@ export default class BoothInfo extends Component {
     ) : null;
 
     return (
-      <Modal
-        animationType="slide"
-        transparent={false}
-        visible={showModal}
-      >
-        <ScrollView style={styles.container}>
-          {closeButton}
-          {title}
-          {image}
-          {description}
-          {igp}
-          {website}
-          {courses}
-          <List>{faculties}</List>
-        </ScrollView>
-      </Modal>
+      <ScrollView style={styles.container}>
+        {closeButton}
+        {title}
+        {image}
+        {description}
+        {igp}
+        {website}
+        {courses}
+      </ScrollView>
     )
   }
 }
@@ -158,5 +253,8 @@ const styles = StyleSheet.create({
   },
   desc: {
     textAlign: "justify"
+  },
+  link: {
+    color: "#2980b9"
   }
 });
